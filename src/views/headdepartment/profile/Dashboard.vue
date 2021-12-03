aaca60015cd690cf9b80dacf290d098b66c83678
unknown <royrmagdaong@gmail.com>
2021-12-04 01:17:07 +0800

    department: dashboard simple design. static picture only

--- src/views/headdepartment/profile/Dashboard.vue
+++ src/views/headdepartment/profile/Dashboard.vue
@@ -1,8 +1,34 @@
 <template>
-  <div>
-      <div class="h3">Dashboard</div>
+  <div class="px-5 py-4 card shadow-sm">
       
-      <img :src="signature" alt="Signature">
+    <div class="d-md-none my-4">
+      <div style="width:120px;height:120px;;border-radius:50%;position:relative;" class="mx-auto d-block">
+        <img class="prof-pic" :src="`https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg`" />
+      </div>
+      <div class="d-flex justify-content-center">
+        <img :src="signature" alt="Signature" style="width:160px; height:auto;">
+      </div>
+      <div class="d-flex align-items-center justify-content-center">
+        <div class="h3 mb-2">{{ get(department, 'department_name') }}</div>
+        <b-icon class="edit-info ml-2" icon="pencil" variant="primary" font-scale="1"></b-icon>
+      </div>
+      <div class="text-center">{{ get(department, 'in_charge') }}</div>
+      <div class="text-center">{{ get(department, 'email') }}</div>
+    </div>
+
+    <div class="d-none d-md-block">
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="h3 mb-2">{{ get(department, 'department_name') }}</div>
+        <b-icon class="edit-info" icon="pencil" variant="primary" font-scale="1"></b-icon>
+      </div>
+      <div class="">{{ get(department, 'in_charge') }}</div>
+      <div class="">{{ get(department, 'email') }}</div>
+      <div>
+        <span class="mr-4">Signature</span>
+        <img :src="signature" alt="Signature" style="width:160px; height:auto;">
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -52,6 +78,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.prof-pic{
+  background-color: #eee !important;
+  height: 120px;
+  width: 120px;
+  border-radius: 60px;
+}
+.prof-pic:hover{
+  opacity: 0.4;
+  cursor: pointer;
+}
 </style>
